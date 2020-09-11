57b383fc651ab273bd21fc62703de5007f0b03e8
JonnyCSantos <jonathandossantos.c@gmail.com>
2020-09-11 13:09:41 -0300

    feat: menu mobile + missao

---
+++ assets/src/js/menu-mobile.js
@@ -0,0 +1,9 @@
+$('.hamburguer-menu').click(function() {
+    $('body').addClass('menu-open')
+})
+$('.close-menu').click(function() {
+    $('body').removeClass('menu-open')
+})
+$('.mask-menu').click(function() {
+    $('body').removeClass('menu-open')
+})
